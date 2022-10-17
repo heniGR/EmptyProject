88e6ab8aecea2855ebb7f8d1b4bd12aef60fa0d4
unknown <heni.guirat@esprit.tn>
2022-10-17 20:40:51 +0100

    pipeline1

---
+++ src/test/java/com/esprit/examen/controllers/StockRestControllerTest.java
@@ -0,0 +1,52 @@
+package com.esprit.examen.controllers;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.esprit.examen.repositories.StockRepository;
+import com.esprit.examen.services.StockServiceImpl;
+import com.esprit.examen.entities.Stock;
+
+@SpringBootTest
+class StockRestControllerTest {
+	
+	@Autowired
+	StockServiceImpl stockImpl;
+	@Test
+	void testGetStocks() {
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetrieveStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddStock() {
+	//	List<Stock> stocks = stockService.retrieveAllStocks();
+	//	int expected=stocks.size();
+		Stock s = new Stock(10L,"stock test",100,20,null);
+		Stock savedStock= stockImpl.addStock(s);
+		
+	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
+		assertNotNull(savedStock.getLibelleStock());
+		//stockImpl.deleteStock(savedStock.getIdStock());
+	}
+
+	@Test
+	void testRemoveStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testModifyStock() {
+		fail("Not yet implemented");
+	}
+
+}
