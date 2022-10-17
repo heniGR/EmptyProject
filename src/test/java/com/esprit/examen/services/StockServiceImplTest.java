252c472b206ddbae2ec97230c5e1ebac4c613c9a
unknown <heni.guirat@esprit.tn>
2022-10-17 19:10:02 +0100

    pipeline

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -3,13 +3,15 @@ package com.esprit.examen.services;
 import static org.junit.Assert.*;
 import java.util.List;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class StockServiceImplTest {
 	@Autowired
